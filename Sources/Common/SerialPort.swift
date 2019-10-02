14854fc7caaaa895830663c8ae320d639cc66efe
Pongsak Suvanpong <psksvp@gmail.com>
2019-10-02 18:37:04 +1000

    add SerialPort untested

---
+++ Sources/Common/SerialPort.swift
@@ -0,0 +1,200 @@
+/*
+*  The BSD 3-Clause License
+*  Copyright (c) 2018. by Pongsak Suvanpong (psksvp@gmail.com)
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without modification,
+*  are permitted provided that the following conditions are met:
+*
+*  1. Redistributions of source code must retain the above copyright notice,
+*  this list of conditions and the following disclaimer.
+*
+*  2. Redistributions in binary form must reproduce the above copyright notice,
+*  this list of conditions and the following disclaimer in the documentation
+*  and/or other materials provided with the distribution.
+*
+*  3. Neither the name of the copyright holder nor the names of its contributors may
+*  be used to endorse or promote products derived from this software without
+*  specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+*  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+*  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+*  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+*  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* This information is provided for personal educational purposes only.
+*
+* The author does not guarantee the accuracy of this information.
+*
+* By using the provided information, libraries or software, you solely take the risks of damaging your hardwares.
+*/
+ 
+import Foundation
+
+#if os(OSX)
+import Darwin.C
+#elseif os(Linux)
+import Glibc
+#endif
+ 
+public class SerialPort
+{
+  public enum Baud
+  {
+    case b1200
+    case b2400
+    case b4800
+    case b9600
+    case b19200
+    case b115200
+    
+    var value: speed_t
+    {
+      get
+      {
+        switch self
+        {
+          case .b1200   : return tcflag_t(B1200)
+          case .b2400   : return tcflag_t(B2400)
+          case .b4800   : return tcflag_t(B4800)
+          case .b9600   : return tcflag_t(B9600)
+          case .b19200  : return tcflag_t(B19200)
+          case .b115200 : return tcflag_t(B115200)
+        }
+      }
+    }
+  }
+  
+  public enum BitSize: tcflag_t
+  {
+    case five
+    case six
+    case seven
+    case eight
+    
+    var value: tcflag_t
+    {
+      get
+      {
+        switch self
+        {
+          case .five  : return tcflag_t(CS5)
+          case .six   : return tcflag_t(CS6)
+          case .seven : return tcflag_t(CS7)
+          case .eight : return tcflag_t(CS8)
+        }
+      }
+    }
+  }
+  
+  public enum Parity: tcflag_t
+  {
+    case none
+    case even
+    case odd
+    
+    var value: tcflag_t
+    {
+      get
+      {
+        switch self
+        {
+          case .none : return tcflag_t(0)
+          case .even : return tcflag_t(PARENB)
+          case .odd  : return tcflag_t(PARENB | PARODD)
+        }
+      }
+    }
+  }
+  
+  public enum Error
+  {
+    case openPortFail
+    case notSerialPort
+  }
+  
+  private let fileID:Int32
+  
+  public init(path: String, baud: Baud = .b9600,
+                            bitSize: BitSize = .eight,
+                            parity: Parity = .none)
+  {
+#if os(macOS)
+    self.fileID = open(path, O_RDWR | O_NOCTTY | O_EXLOCK)
+#elseif os(Linux)
+    self.fileID = open(path, O_RDWR | O_NOCTTY)
+#endif
+    if(-1 == self.fileID)
+    {
+      Log.error("\(self) fail to open serial port at \(path)")
+    }
+    else
+    {
+      var config = termios()
+      tcgetattr(fileID, &config)
+      cfsetspeed(&config, baud.value)
+      config.c_cflag |= parity.value
+      config.c_cflag &= ~tcflag_t(CSTOPB) // no stop bit
+      
+      config.c_cflag &= ~tcflag_t(CSIZE)
+      config.c_cflag |= bitSize.value
+      
+      tcsetattr(fileID, TCSANOW, &config)
+    }
+  }
+  
+  deinit
+  {
+    if -1 != fileID
+    {
+      close(fileID)
+    }
+  }
+  
+  
+  public func read(size: Int) -> [UInt8]?
+  {
+    if(-1 == fileID)
+    {
+      return nil
+    }
+    else
+    {
+      var rawBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
+      defer { rawBuffer.deallocate() }
+      
+      let bytesRead = SwiftGlibc.read(fileID, rawBuffer, size)
+      if(bytesRead > 0)
+      {
+        return Array(UnsafeBufferPointer(start: rawBuffer, count: bytesRead))
+      }
+      else
+      {
+        return nil
+      }
+    }
+  }
+  
+  public func write(data: [UInt8]) -> Int
+  {
+    if(-1 == fileID)
+    {
+      return 0
+    }
+    else
+    {
+      let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: data.count)
+      var d = data // the feak below wants a fucking var.
+      buffer.initialize(from: &d, count: data.count)
+      defer { buffer.deallocate() }
+      let bytesWritten = SwiftGlibc.write(fileID, buffer, data.count)
+      return bytesWritten
+    }
+  }
+}
---
+++ Sources/PiHardwareInterface/HMC5883L.swift
@@ -0,0 +1,103 @@
+/**
+  * Created by psksvp@gmail.com on 3/12/17.
+  * Based on https://bitbucket.org/thinkbowl/i2clibraries/src/037e6107a866eb6406fced4d0fbef197f2f131a0/i2c_hmc5883l.py?at=master&fileviewer=file-view-default
+  */
+
+#if os(Linux) && arch(arm)
+
+import Glibc
+
+
+class HMC5883L
+{
+  private let address:Int
+  private let declinationDegree:Float
+  private let declinationMin:Float
+  private let gauss:Float
+  private let declination:Float
+  
+  private let ConfigurationRegisterA :Int32 = 0x00
+  private let ConfigurationRegisterB :Int32 = 0x01
+  private let ModeRegister: Int32 = 0x02
+  private let AxisXDataRegisterMSB :Int32 = 0x03
+  private let AxisXDataRegisterLSB :Int32 = 0x04
+  private let AxisZDataRegisterMSB :Int32 = 0x05
+  private let AxisZDataRegisterLSB :Int32 = 0x06
+  private let AxisYDataRegisterMSB :Int32 = 0x07
+  private let AxisYDataRegisterLSB :Int32 = 0x08
+  private let StatusRegister:UInt8 = 0x09
+  private let IdentificationRegisterA:UInt8 = 0x10
+  private let IdentificationRegisterB:UInt8 = 0x11
+  private let IdentificationRegisterC:UInt8 = 0x12
+  private let MeasurementContinuous:UInt8 = 0x00
+  private let MeasurementSingleShot:UInt8 = 0x01
+  private let MeasurementIdle:UInt8 = 0x03
+  
+  private let scaleMap = [0.88: (0x00, 0.73),
+                          1.3 : (0x01, 0.92),
+                          1.9 : (0x02, 1.22),
+                          2.5 : (0x03, 1.52),
+                          4.0 : (0x04, 2.27),
+                          4.7 : (0x05, 2.56),
+                          5.6 : (0x06, 3.03),
+                          8.1 : (0x07, 4.35)]
+                          
+ 
+  private let i2c: I2CBus
+  private let scaleReg: UInt8
+  private let scale: Float
+  
+  init(address:Int=0x1e, declinationDegree:Float=0, declinationMin:Float=6, gauss:Float=1.3)
+  {
+    //guard i2c.isReachable(address) else { }
+    self.address = address
+    self.declinationDegree = declinationDegree
+    self.declinationMin = declinationMin
+    self.gauss = gauss
+    self.declination = (declinationDegree + declinationMin / 60.0) * (Float.pi / 180.0)
+    self.scaleReg = UInt8(0x01 << 5)
+    self.scale = 0.92
+    
+    self.i2c = I2CBus(deviceAddress: Int32(address))
+    i2c.writeByte(register: ConfigurationRegisterB, data: scaleReg);
+    i2c.writeByte(register: ModeRegister, data: MeasurementContinuous)
+  }
+  
+  func read() -> (Float, Float, Float)
+  {
+    let msbx = i2c.readByte(register: AxisXDataRegisterMSB)
+    let lsbx = i2c.readByte(register: AxisXDataRegisterLSB)
+    let msbz = i2c.readByte(register: AxisZDataRegisterMSB)
+    let lsbz = i2c.readByte(register: AxisZDataRegisterLSB)
+    let msby = i2c.readByte(register: AxisYDataRegisterMSB)
+    let lsby = i2c.readByte(register: AxisYDataRegisterLSB)
+    
+    let magx:UInt16 = UInt16(msbx << 8 | lsbx & 0xff)
+    let magz:UInt16 = UInt16(msbz << 8 | lsbz & 0xff)
+    let magy:UInt16 = UInt16(msby << 8 | lsby & 0xff)
+    return (Float(magx) * scale, Float(magy) * scale, Float(magz) * scale)
+  }
+  
+  func heading() -> (Float, Float)
+  {
+    let (x, y, _) = read()
+    var headingRad = atan2(y, x)
+    headingRad += declination
+    if(headingRad < 0)
+    {
+      headingRad += (2 * Float.pi)
+    }
+
+    if(headingRad >= 2 * Float.pi)
+    {
+      headingRad -= (2 * Float.pi)
+    }
+
+    let headingDeg = headingRad * 180.0 / Float.pi
+    let degrees = floor(headingDeg)
+    let minutes = round((headingDeg - degrees) * 60.0)
+    return (Float(degrees), Float(minutes))
+  } 
+}
+
+#endif
